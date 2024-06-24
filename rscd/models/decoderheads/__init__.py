84bd02dc27caa946dcae636d0904db1e13060e06
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2024-06-24 16:32:21 +0800

    Update __init__.py

--- rscd/models/decoderheads/__init__.py
+++ rscd/models/decoderheads/__init__.py
@@ -1,2 +1,3 @@
 from rscd.models.decoderheads.stnet import STNet
 from rscd.models.decoderheads.MaskFormerModel_sea_ourDH import MaskFormerModel_sea_ourDH
+from rscd.models.decoderheads.DDLNet import DDLNet
