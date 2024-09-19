7197716afa5b2e8ef97eefd1c0ca8bc35ec0d8df
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2024-09-19 16:45:02 +0800

    Update __init__.py

--- rscd/models/decoderheads/__init__.py
+++ rscd/models/decoderheads/__init__.py
@@ -7,3 +7,7 @@ from rscd.models.decoderheads.SNUNet_ECAM import ECAM
 from rscd.models.decoderheads.CFde import ChangeFormer_DE
 from rscd.models.decoderheads.lgpnet_b import LGPNet_b
 from rscd.models.decoderheads.SARASNet import Change_detection
+from rscd.models.decoderheads.AFCF3D_de import AFCD3D_decoder
+from rscd.models.decoderheads.USSFCNet import USSFCNet_decoder
+
+from rscd.models.decoderheads.xformer import CDXLSTM
