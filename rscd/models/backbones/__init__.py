65f9fd5e56865e829f054c229ffd727b9a10b9b0
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2024-09-19 16:42:24 +0800

    Update __init__.py

--- rscd/models/backbones/__init__.py
+++ rscd/models/backbones/__init__.py
@@ -5,3 +5,5 @@ from rscd.models.backbones.SNUNet_UNetpp import UNetpp
 from rscd.models.backbones.CFen import ChangeFormer_EN
 from rscd.models.backbones.lgpnet_a import LGPNet_a
 from rscd.models.backbones.SARASNet import SARASNet_backbone
+from rscd.models.backbones.AFCF3D_en import AFCD3D_backbone
+from rscd.models.backbones.USSFCNet import USSFCNet_encoder
